18843b6fdf6bd365b7d886bf5e4b0e852f29e6cb
bhari eutsey <bharilynn@outlook.com>
2021-11-28 19:38:08 -0600

    quiz box completed

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// rule list
+  // You'll have <span>15 seconds </span> to answer each questiom
+  // Any wrong answer will deduct 2 seconds off the clock
+  // You can't exit the quiz while playing
+  // You can't change your answer once selected
+
+
+
+
+
+
+ // javascript questions
+  // 1 Inside which HTML element do we put the JavaScript?
+         //a- &lt;scripting&gt
+         //b- &lt;javascript&gt
+         //c- &lt;script&gt
+         //d- &lt;js&gt
+             // ans: c
+
+  // 2 How do you get the DOM element with id in JavaScript?
+        // a- window.getElementById(...)   
+        // b- document.getElementById(...) 
+        // c- page.getElementById(...) 
+        // d- document.innerHTML.getElementById(...) 
+            // ans : a
+    // 3 Using _______ statement is how you test for a specific condition.
+         // a- Select
+         // b- If
+         // c- Switch
+         // d- For
+             // ans: b
+         
+    //  4 In Javascript, Window.prompt() method returns a true or false value.
+           // a- False
+           // b- True 
+             // ans: a 
+     
+    //  5 Which is the correct syntax for displaying data in the console?
+           // a- console.log();
+           // b- log.console();
+           // c- console.log[];
+           // d- console.log; 
+               // ans: a
